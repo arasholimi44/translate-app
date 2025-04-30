11881fd914b91be40281ce86a5a2858410bea8ee
arasholimi44 <arashalimii@gmail.com>
2025-04-30 16:11:47 -0500

    Fix: cleaned up login page (removed unused router import)

--- frontend/apps/src/app/user/page.tsx
+++ frontend/apps/src/app/user/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { getSession, signIn, signOut } from 'next-auth/react';
-import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import Link from "next/link";
 //import { getCurrentUser } from 'aws-amplify/auth';
